397e43625632229f67e9eeb8529fe9e35b861443
KernelPlusAcademy <KernelPlusAcademy@gmail.com>
2025-05-30 21:21:37 -0400

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,12 @@
-// DO NOT use "import" in browser JS unless you're using a bundler like Webpack/Vite
-
-// Replace this with your actual config
 const firebaseConfig = {
   apiKey: "AIzaSyA4uVJOZ1ZWls6hK1b94Bm-waDgyJfYehM",
   authDomain: "shellcoach-eacb2.firebaseapp.com",
   projectId: "shellcoach-eacb2",
-  storageBucket: "shellcoach-eacb2.appspot.com", // fixed typo here: use 'appspot.com'
+  storageBucket: "shellcoach-eacb2.appspot.com",
   messagingSenderId: "97519723209",
   appId: "1:97519723209:web:5d815580c3d64b0f486318",
   measurementId: "G-LJZYMRCCCE"
 };
 
+// Use compatibility syntax for browser projects
 firebase.initializeApp(firebaseConfig);
